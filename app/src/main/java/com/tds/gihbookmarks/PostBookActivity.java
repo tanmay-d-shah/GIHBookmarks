9fdc35f4891f275f5b39dde765d34296cb0f1876
TanmayDineshkumarShah <tanmayshah314@gmail.com>
2019-12-29 18:38:33 +0530

    Added Post Book Activity

---
+++ app/src/main/java/com/tds/gihbookmarks/PostBookActivity.java
@@ -0,0 +1,215 @@
+package com.tds.gihbookmarks;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.tds.gihbookmarks.model.Book;
+import com.tds.gihbookmarks.util.UserApi;
+
+import java.util.Date;
+
+public class PostBookActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int GALLERY_CODE = 1;
+    private Button addBookButton;
+    private ImageView bookImg1;
+
+    private EditText titleText;
+    private EditText authorText;
+    private EditText publicationText;
+    private EditText editionText;
+    private EditText priceText;
+
+    private String currentUserId;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+    private FirebaseFirestore db=FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
+    private CollectionReference collectionReference=db.collection("Books");
+
+
+    private Uri img1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_book);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        storageReference= FirebaseStorage.getInstance().getReference();
+
+        titleText=findViewById(R.id.title_text);
+        authorText=findViewById(R.id.author_text);
+        publicationText=findViewById(R.id.publication_text);
+        editionText=findViewById(R.id.edition_text);
+        priceText=findViewById(R.id.price_text);
+
+        bookImg1=findViewById(R.id.book_img1);
+
+
+        addBookButton=findViewById(R.id.add_book);
+
+        addBookButton.setOnClickListener(this);
+
+
+
+
+        bookImg1.setOnClickListener(this);
+
+        if(UserApi.getInstance()!=null){
+            currentUserId=UserApi.getInstance().getUserId();
+
+        }
+        authStateListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user=firebaseAuth.getCurrentUser();
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user=firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(firebaseAuth!=null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.add_book:
+                uploadBook();
+                break;
+            case R.id.book_img1:
+                Intent GalleryIntent=new Intent(Intent.ACTION_GET_CONTENT);
+                GalleryIntent.setType("image/*");
+                startActivityForResult(GalleryIntent,GALLERY_CODE);
+                break;
+
+        }
+    }
+
+    private void uploadBook() {
+        final String title=titleText.getText().toString().trim();
+        final String author=authorText.getText().toString().trim();
+        final String publication=publicationText.getText().toString().trim();
+        final String edition=editionText.getText().toString().trim();
+        final String price=priceText.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(title)
+           && !TextUtils.isEmpty(publication)
+           && !TextUtils.isEmpty(edition)
+           && !TextUtils.isEmpty(author)
+           && !TextUtils.isEmpty(price)
+           && img1!=null
+
+        ){
+
+
+                final StorageReference filepath=storageReference
+                        .child("book_images")
+                        .child("image"+ Timestamp.now().getSeconds());
+                filepath.putFile(img1)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        String imgageUrl=uri.toString();
+                                        Book book=new Book();
+                                        book.setImageUrl1(imgageUrl);
+                                        book.setTitle(title);
+                                        book.setAuthor(author);
+                                        book.setEdition(edition);
+                                        book.setExpectedPrice(price);
+                                        book.setPublication(publication);
+                                        book.setUserId(currentUserId);
+
+                                        book.setDateAdded(new Timestamp(new Date()));
+
+                                        collectionReference.add(book)
+                                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                    @Override
+                                                    public void onSuccess(DocumentReference documentReference) {
+                                                        startActivity(new Intent(PostBookActivity.this,HomepageActivity.class));
+                                                        finish();
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Log.d("PostBookActivity", "onFailure: "+e.getMessage());
+                                                    }
+                                                });
+
+
+                                    }
+                                });
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("PostBookActivity", "onFailure: "+e.getMessage());
+                            }
+                        });
+
+
+
+
+
+        }
+        else{
+            Snackbar.make(findViewById(R.id.post_book_layout),"Empty Fields",Snackbar.LENGTH_LONG).setAction("Action",null).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GALLERY_CODE && resultCode==RESULT_OK){
+            if(data!=null){
+                img1=data.getData();
+                bookImg1.setImageURI(img1);
+            }
+        }
+    }
+}
--- app/src/main/java/com/tds/gihbookmarks/model/Book.java
+++ app/src/main/java/com/tds/gihbookmarks/model/Book.java
@@ -1,4 +1,118 @@
 package com.tds.gihbookmarks.model;
 
+import com.google.firebase.Timestamp;
+
 public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private String publication;
+    private String edition;
+    private String expectedPrice;
+    private String city;
+    private String userId;
+    private String imageUrl1;
+
+    private Timestamp dateAdded;
+
+    public Book(){}
+
+    public Book(String isbn, String title, String author, String publication, String edition, String expectedPrice, String city, String userId, String imageUrl1, String imageUrl2, Timestamp dateAdded) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publication = publication;
+        this.edition = edition;
+        this.expectedPrice = expectedPrice;
+        this.city = city;
+        this.userId = userId;
+        this.imageUrl1 = imageUrl1;
+
+        this.dateAdded = dateAdded;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublication() {
+        return publication;
+    }
+
+    public void setPublication(String publication) {
+        this.publication = publication;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public String getExpectedPrice() {
+        return expectedPrice;
+    }
+
+    public void setExpectedPrice(String expectedPrice) {
+        this.expectedPrice = expectedPrice;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getImageUrl1() {
+        return imageUrl1;
+    }
+
+    public void setImageUrl1(String imageUrl1) {
+        this.imageUrl1 = imageUrl1;
+    }
+
+
+
+    public Timestamp getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Timestamp dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+
 }
+
