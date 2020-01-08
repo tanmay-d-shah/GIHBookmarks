605cbd1e57293edd6daed82845f719101e471d8e
parthagravat <parthagravat8080@gmail.com>
2020-01-08 20:45:24 +0530

    login,signin,sell activity

--- app/src/main/java/com/tds/gihbookmarks/SignInActivity.java
+++ app/src/main/java/com/tds/gihbookmarks/SignInActivity.java
@@ -25,7 +25,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.tds.gihbookmarks.util.UserApi;
 
-public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignInActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore db=FirebaseFirestore.getInstance();
     private CollectionReference collectionReference=db.collection("Users");
@@ -39,85 +39,6 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
-        firebaseAuth=FirebaseAuth.getInstance();
-
-        loginEmail=findViewById(R.id.login_email_text);
-        loginPass=findViewById(R.id.login_pass_text);
-        loginButton=findViewById(R.id.login_button);
-        signUpButton=findViewById(R.id.signup_button);
-
-        loginButton.setOnClickListener(this);
-        signUpButton.setOnClickListener(this);
-
-
-
-
-
-
-
-
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.signup_button:
-                startActivity(new Intent(SignInActivity.this,CreateAccountActivity.class));
-                break;
-            case R.id.login_button:
-                login(loginEmail.getText().toString().trim(),loginPass.getText().toString().trim());
-        }
-    }
-
-    private void login(String email, String pass) {
-        if(!TextUtils.isEmpty(email)
-        && !TextUtils.isEmpty(pass)){
-            firebaseAuth.signInWithEmailAndPassword(email,pass)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            final FirebaseUser user=firebaseAuth.getCurrentUser();
-                            assert user!=null;
-
-                            if(user.isEmailVerified()){
-                            final String currentUserId=user.getUid();
-                            collectionReference
-                                    .whereEqualTo("UserId",currentUserId)
-                                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                                        @Override
-                                        public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                                            assert queryDocumentSnapshots!=null;
-                                                if(!queryDocumentSnapshots.isEmpty()){
-                                                    for(QueryDocumentSnapshot snapshot:queryDocumentSnapshots){
-                                                        UserApi userApi=UserApi.getInstance();
-                                                        userApi.setUserId(snapshot.getString("UserId"));
-                                                        userApi.setUsername(snapshot.getString("Name"));
-                                                        userApi.setUserMobile(snapshot.getString("Mobile"));
-
-                                                        startActivity(new Intent(SignInActivity.this,StationaryPostActivity.class));
-                                                    }
-
-
-
-                                                }
-
-                                        }
-                                    });
-                            }
-                            else{
-
-                                Snackbar.make(findViewById(R.id.LinearLayout),"Please verify your Email",Snackbar.LENGTH_LONG).setAction("Action",null).show();
-
-                            }
-
-                    }
-            });
-        }
-        else{
-
-        }
 
     }
 }
---
+++ app/src/main/java/com/tds/gihbookmarks/login.java
@@ -0,0 +1,24 @@
+package com.tds.gihbookmarks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onClick(View view)
+    {
+
+    Intent i =new Intent(this,SignInActivity.class);
+    startActivity(i);
+
+    }
+}
