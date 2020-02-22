d2d7b47ddf212c7862b8a59fd1e494c9ca0ce381
KhyatiVyas13 <vyaskhyati148@gmail.com>
2020-02-22 19:47:35 +0530

    Sample FAQs added(static)

--- app/src/main/java/com/tds/gihbookmarks/SellAllActivity.java
+++ app/src/main/java/com/tds/gihbookmarks/SellAllActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ToolPostActivity extends AppCompatActivity {
+public class SellAllActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tool_post);
+        setContentView(R.layout.activity_sell_all);
     }
 }
