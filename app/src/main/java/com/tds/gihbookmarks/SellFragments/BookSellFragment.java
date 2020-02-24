3b58d25e7206addb56fd29b9086e49bf6d3dbaef
KhyatiVyas13 <vyaskhyati148@gmail.com>
2020-02-24 23:07:22 +0530

    views(UI components) declared in java files(for forms)

--- app/src/main/java/com/tds/gihbookmarks/SellFragments/BookSellFragment.java
+++ app/src/main/java/com/tds/gihbookmarks/SellFragments/BookSellFragment.java
@@ -9,11 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.tds.gihbookmarks.R;
 
 public class BookSellFragment extends Fragment {
 
+    private ImageView imgBook;
+    private TextInputEditText name,author,price,edition,publication;
+    private Button btnSell;
     public BookSellFragment(){
 
     }
@@ -22,7 +28,16 @@ public class BookSellFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_sell, container, false);
+
+        View view= inflater.inflate(R.layout.fragment_book_sell, container, false);
+        imgBook=view.findViewById(R.id.imgBook);
+        name=view.findViewById(R.id.book_name);
+        author=view.findViewById(R.id.book_writer);
+        price=view.findViewById(R.id.book_price);
+        edition=view.findViewById(R.id.book_edition);
+        publication=view.findViewById(R.id.book_publication);
+        btnSell=view.findViewById(R.id.btnSell);
+        return view;
     }
 
 }
--- app/src/main/java/com/tds/gihbookmarks/SellFragments/ToolSellFragment.java
+++ app/src/main/java/com/tds/gihbookmarks/SellFragments/ToolSellFragment.java
@@ -12,9 +12,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.tds.gihbookmarks.R;
 
 import java.util.ArrayList;
@@ -22,8 +25,12 @@ import java.util.List;
 
 public class ToolSellFragment extends Fragment {
 
-    private Spinner spinner;
+    //private Spinner spinner;
     //private View view;
+
+    private ImageView imgTools;
+    private TextInputEditText name,desc,price;
+    private Button btn_sell;
 
     public ToolSellFragment() {
         // Required empty public constructor
@@ -34,8 +41,12 @@ public class ToolSellFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         final View view = inflater.inflate(R.layout.fragment_tool_sell, container, false);
+        imgTools=view.findViewById(R.id.imgTool);
+        name=view.findViewById(R.id.tool_name);
+        desc=view.findViewById(R.id.tool_desc);
+        price=view.findViewById(R.id.tool_price);
+        btn_sell=view.findViewById(R.id.btnSellTool);
         /*spinner=view.findViewById(R.id.toolList);
-
 
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(view.getContext(),R.array.tools, layout.simple_spinner_item);
         arrayAdapter.setDropDownViewResource(layout.simple_spinner_dropdown_item);
