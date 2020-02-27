26d5c7451207ca32937eca38d99df773cad98fe2
KhyatiVyas13 <vyaskhyati148@gmail.com>
2020-02-27 13:15:28 +0530

    FAQ questions activity added

---
+++ app/src/main/java/com/tds/gihbookmarks/faq_1Activity.java
@@ -0,0 +1,14 @@
+package com.tds.gihbookmarks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class faq_1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq_1);
+    }
+}
