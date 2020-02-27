4036e0af995f62ebb332e25cc994aab943de4632
KhyatiVyas13 <vyaskhyati148@gmail.com>
2020-02-27 22:07:33 +0530

    blank fragments for Knowledge activity added

--- app/src/main/java/com/tds/gihbookmarks/NavigationMenuFragments/KnowledgeFragment.java
+++ app/src/main/java/com/tds/gihbookmarks/NavigationMenuFragments/KnowledgeFragment.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+import com.tds.gihbookmarks.PageAdapter_HomePage;
+import com.tds.gihbookmarks.PageAdapter_KnowledgeFragment;
 import com.tds.gihbookmarks.R;
 
 public class KnowledgeFragment extends Fragment {
@@ -21,6 +24,7 @@ public class KnowledgeFragment extends Fragment {
     private TabLayout KnowledgetabLayout;
     private TabItem info_notes,links,e_book,project;
     private ViewPager viewPager_knowledge;
+    private PageAdapter_KnowledgeFragment pageAdapter;
 
     public KnowledgeFragment() {
         // Required empty public constructor
@@ -40,6 +44,21 @@ public class KnowledgeFragment extends Fragment {
         project = view.findViewById(R.id.project_hiring);
 
         viewPager_knowledge = view.findViewById(R.id.KnowledgeViewpager);
+
+        pageAdapter = new PageAdapter_KnowledgeFragment(getChildFragmentManager(), KnowledgetabLayout.getTabCount());
+        viewPager_knowledge.setAdapter(pageAdapter);
+        KnowledgetabLayout.setupWithViewPager(viewPager_knowledge);
+
+        try {
+            KnowledgetabLayout.getTabAt(0).setText("Informative Posts");
+            KnowledgetabLayout.getTabAt(1).setText("Links");
+            KnowledgetabLayout.getTabAt(2).setText("E-Books(PDF)");
+            KnowledgetabLayout.getTabAt(3).setText("Project Hiring");
+
+        } catch (Exception e) {
+            Log.i("Khyati1234", e.toString());
+        }
+
         return view;
     }
 
--- app/src/main/java/com/tds/gihbookmarks/PageAdapter_KnowledgeFragment.java
+++ app/src/main/java/com/tds/gihbookmarks/PageAdapter_KnowledgeFragment.java
@@ -1,4 +1,68 @@
 package com.tds.gihbookmarks;
 
-public class PageAdapter_KnowledgeFragment {
+import android.util.Log;
+
+import com.tds.gihbookmarks.HomePageFragments.BooksFragment;
+import com.tds.gihbookmarks.HomePageFragments.OtherFragment;
+import com.tds.gihbookmarks.HomePageFragments.StudyMaterialFragment;
+import com.tds.gihbookmarks.HomePageFragments.ToolsFragment;
+import com.tds.gihbookmarks.KnowledgeFragmentsTabs.EBooksFragment;
+import com.tds.gihbookmarks.KnowledgeFragmentsTabs.InformativePostFragment;
+import com.tds.gihbookmarks.KnowledgeFragmentsTabs.LinksFragment;
+import com.tds.gihbookmarks.KnowledgeFragmentsTabs.ProjectHiringFragment;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import static androidx.viewpager.widget.PagerAdapter.POSITION_NONE;
+
+public class PageAdapter_KnowledgeFragment extends FragmentPagerAdapter {
+    private  int numoftabs;
+
+    public PageAdapter_KnowledgeFragment(@NonNull FragmentManager fm, int numOfTabs){
+        super(fm,numOfTabs);
+        this.numoftabs = numOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = new Fragment();
+        Log.d("HomePage", "getItem: HomePAge" + position);
+        switch (position) {
+            case 0:
+                fragment = new InformativePostFragment();
+                return fragment;
+
+            case 1:
+                fragment = new LinksFragment();
+                return fragment;
+
+            case 2:
+                fragment = new EBooksFragment();
+                return fragment;
+
+            case 3:
+                fragment = new ProjectHiringFragment();
+                return fragment;
+
+            default:
+                return fragment;
+
+        }
+
+
+    }
+
+    @Override
+    public int getCount() {
+        return numoftabs;
+    }
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return POSITION_NONE;
+    }
 }
