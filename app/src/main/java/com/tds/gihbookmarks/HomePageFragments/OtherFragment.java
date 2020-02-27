bee6195a6449c40b16631bc098f280880e33a9a2
Tirth <tirthtrivedi17@gmail.com>
2020-02-27 21:39:39 +0530

    Commited to learn

--- app/src/main/java/com/tds/gihbookmarks/HomePageFragments/OtherFragment.java
+++ app/src/main/java/com/tds/gihbookmarks/HomePageFragments/OtherFragment.java
@@ -1,13 +1,35 @@
 package com.tds.gihbookmarks.HomePageFragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.StorageReference;
 import com.tds.gihbookmarks.R;
+import com.tds.gihbookmarks.StaggeredRecyclerViewAdapter;
+import com.tds.gihbookmarks.model.SaleItems;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class OtherFragment extends Fragment {
@@ -15,6 +37,21 @@ public class OtherFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private View view;
+    private RecyclerView bookRecyclerView;
+//    private ArrayList<String> mName;
+
+    private List<SaleItems> saleItemsList;
+    //    private ArrayList<String> mImageUrls;
+    private StaggeredRecyclerViewAdapter staggeredRecyclerViewAdapter;
+    private int NUM_COLUMNS = 2;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseUser user;
+    private StorageReference storageReference;
+    private CollectionReference collectionReference = db.collection("SaleItems");
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -46,17 +83,77 @@ public class OtherFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        collectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot books : task.getResult()) {
+                        SaleItems items = books.toObject(SaleItems.class);
+                        if (items.getItem().equals("Book") && items.getStatus().equals("Available")) {
+                            saleItemsList.add(items);
+                        }
+//                                items.setBookId(books.getId());
+                    }
+                    Log.d("Restart", "onComplete: Books Restarted");
+                    Log.d("hi", "onComplete: Hi");
+                    staggeredRecyclerViewAdapter = new StaggeredRecyclerViewAdapter(getContext(), saleItemsList);
+
+                    bookRecyclerView.setAdapter(staggeredRecyclerViewAdapter);
+                    staggeredRecyclerViewAdapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_other, container, false);
+
+        view = inflater.inflate(R.layout.fragment_other, container, false);
+        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        saleItemsList = new ArrayList<>();
+        bookRecyclerView = (RecyclerView) view.findViewById(R.id.rentrecyclerView);
+        //StaggeredRecyclerViewAdapter staggeredRecyclerViewAdapter= new StaggeredRecyclerViewAdapter(getContext(),bookList);
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(NUM_COLUMNS, LinearLayoutManager.VERTICAL);
+        //bookRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        bookRecyclerView.setLayoutManager(staggeredGridLayoutManager);
+        bookRecyclerView.setAdapter(staggeredRecyclerViewAdapter);
+
+        setHasOptionsMenu(true);
+
+        return view;
     }
 
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(null);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+//                queryFor="books";
+                staggeredRecyclerViewAdapter.getFilter().filter(newText);
+                return false;
+            }
+
+        });
+
+        super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 }
