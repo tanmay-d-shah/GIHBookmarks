5afc2d6b0aa9b29b3d7a866f2c7f24e3815e46de
KhyatiVyas13 <vyaskhyati148@gmail.com>
2020-02-27 23:40:17 +0530

    for pull

--- app/src/main/java/com/tds/gihbookmarks/KnowledgeFragmentsTabs/InformativePostFragment.java
+++ app/src/main/java/com/tds/gihbookmarks/KnowledgeFragmentsTabs/InformativePostFragment.java
@@ -5,12 +5,20 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.tds.gihbookmarks.R;
+import com.tds.gihbookmarks.RecyclerViewAdapterInfo;
+import com.tds.gihbookmarks.StaggeredRecyclerViewAdapter;
+import com.tds.gihbookmarks.model.SaleItems;
+
+import java.util.List;
 
 
 public class InformativePostFragment extends Fragment {
@@ -19,12 +27,21 @@ public class InformativePostFragment extends Fragment {
         // Required empty public constructor
     }
 
+    //private List<SaleItems> saleItemsList;
+    //    private ArrayList<String> mImageUrls;
+    private RecyclerViewAdapterInfo recyclerViewAdapterInfo;
+    private View view;
+    private RecyclerView infoRecycler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_informative_post, container, false);
+        view= inflater.inflate(R.layout.fragment_informative_post, container, false);
+        /*infoRecycler=view.findViewById(R.id.info_recyclerView);
+        infoRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        infoRecycler.setAdapter(recyclerViewAdapterInfo);*/
+        return view;
     }
 
 }
