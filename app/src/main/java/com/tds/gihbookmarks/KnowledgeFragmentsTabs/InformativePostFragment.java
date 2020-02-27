15a1fc6f86196007b02ecea18ff616b3dcc06e2f
KhyatiVyas13 <vyaskhyati148@gmail.com>
2020-02-28 01:05:38 +0530

    informative post layout added

--- app/src/main/java/com/tds/gihbookmarks/KnowledgeFragmentsTabs/InformativePostFragment.java
+++ app/src/main/java/com/tds/gihbookmarks/KnowledgeFragmentsTabs/InformativePostFragment.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Html;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +19,10 @@ import android.widget.LinearLayout;
 import com.tds.gihbookmarks.R;
 import com.tds.gihbookmarks.RecyclerViewAdapterInfo;
 import com.tds.gihbookmarks.StaggeredRecyclerViewAdapter;
+import com.tds.gihbookmarks.model.InformativePost;
 import com.tds.gihbookmarks.model.SaleItems;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -32,6 +37,14 @@ public class InformativePostFragment extends Fragment {
     private RecyclerViewAdapterInfo recyclerViewAdapterInfo;
     private View view;
     private RecyclerView infoRecycler;
+    private List<InformativePost> infos;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        infos=new ArrayList<>();
+        infos.add(new InformativePost("Google Translate Adds Support for 5 New Languages, Including Odia",R.drawable.google));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,8 +52,11 @@ public class InformativePostFragment extends Fragment {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_informative_post, container, false);
         infoRecycler=view.findViewById(R.id.info_recyclerView);
+        recyclerViewAdapterInfo = new RecyclerViewAdapterInfo(getContext(),infos);
         infoRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
         infoRecycler.setAdapter(recyclerViewAdapterInfo);
+
+
         return view;
     }
 
